addd183791a1dd85b0a387a0a7043491bff13add
Rutik wadhavane <rutikwadhavane@gmail.com>
2025-07-08 19:56:15 +0530

    I am just creating a MySyudent class

---
+++ src/main/java/com/student_operation/student_operation/MyStudent.java
@@ -0,0 +1,7 @@
+package com.student_operation.student_operation;
+
+public class MyStudent {
+    public static void main(String[] args) {
+        System.out.println("Hey Core java Student");
+    }
+}
